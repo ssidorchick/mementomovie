b2b6f94965bf8dd5750e85c506125693562818bd
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-08-25 16:23:51 +0300

    Remove 'Thing' mock model related stuff.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,25 +2,7 @@
 
 angular.module('mementoMovieApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
     $http.get('/api/movies').success(function(movies) {
       $scope.movies = movies;
     });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
   });
--- client/app/main/main.controller.spec.js
+++ client/app/main/main.controller.spec.js
@@ -12,7 +12,7 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/things')
+    $httpBackend.expectGET('/api/movies')
       .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);
 
     scope = $rootScope.$new();
@@ -21,8 +21,8 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of things to the scope', function () {
+  it('should attach a list of movies to the scope', function () {
     $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.movies.length).toBe(4);
   });
 });
