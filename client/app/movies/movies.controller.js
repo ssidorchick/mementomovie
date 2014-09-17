bdc8d0de59221519f1427b9fac14872064dca9dc
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-09-17 22:06:49 +0300

    Enable following/unfollowing on movie page.

--- client/app/movies/movies.controller.js
+++ client/app/movies/movies.controller.js
@@ -1,9 +1,32 @@
 'use strict';
 
 angular.module('mementoMovieApp')
-  .controller('MoviesCtrl', function ($scope, $stateParams, Movie) {
+  .controller('MoviesCtrl', function ($scope, $stateParams, Auth, Movie, Profile) {
     Movie.get($stateParams.id)
       .then(function(movie) {
-        $scope.model = movie;
+        if (Auth.isLoggedIn()) {
+          Profile.updateFollowing(movie)
+            .then(function(movie) {
+              $scope.model = movie;
+            });
+        } else {
+          $scope.model = movie;
+        }
       });
+
+    $scope.isLoggedIn = Auth.isLoggedIn;
+
+    $scope.followMovie = function() {
+      Profile.followMovie($scope.model)
+        .then(function() {
+          $scope.model.following = true;
+        });
+    };
+
+    $scope.unfollowMovie = function() {
+      Profile.unfollowMovie($scope.model)
+        .then(function() {
+          $scope.model.following = false;
+        });
+    };
   });
