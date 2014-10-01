a205e39e2d0d0d06b48628c09b858489306a4400
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-10-01 10:24:34 +0300

    Implement client filtering style & markup.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,6 +7,61 @@ angular.module('mementoMovieApp')
         $scope.movies = movies;
       });
 
+    $scope.filters = [
+      {
+        dropdownKey: 'yearFilterMenu',
+        collapseKey: 'isYearFilterCollapsed',
+        title: 'Filter by Year',
+        value: [
+          2014,
+          2015,
+          2016,
+          2017
+        ]
+      },
+      {
+        dropdownKey: 'directorFilterMenu',
+        collapseKey: 'isDirectorFilterCollapsed',
+        title: 'Filter by Director',
+        value: [
+          "Peter Jackson",
+          "Quentine Tarantino",
+          "Christopher Nolan",
+          "Quentine Tarantino 2",
+          "Quentine Tarantino 3",
+          "Christopher Nolan 2",
+          "Christopher Nolan 3",
+          "Anderson"
+        ]
+      },
+      {
+        dropdownKey: 'actorFilterMenu',
+        collapseKey: 'isActorFilterCollapsed',
+        title: 'Filter by Actor',
+        value: [
+          "Leonardo Dicaprio",
+          "Samual L Jackson",
+          "Jonnie Depp",
+          "Leonardo Dicaprio 2",
+          "Samual L Jackson 2",
+          "Jonnie Depp 2",
+          "Samual L Jackson 3",
+          "Jonnie Depp 3",
+          "Bruce Willies"
+        ]
+      }
+    ];
+
+    $scope.filterClick = function(filter) {
+      var prevValue = $scope[filter];
+
+      _.each($scope.filters, function(filter) {
+        $scope[filter.collapseKey] = false;
+      });
+
+      $scope[filter] = !prevValue;
+    };
+
     $scope.hasMoreMovies = function() { return PaginatedMovie.hasMore; };
 
     $scope.followMovie = function(movie) {
