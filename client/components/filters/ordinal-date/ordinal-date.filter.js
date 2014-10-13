19e72171793322cb3cf5031b6d20a3dd7b270957
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-10-13 17:16:25 +0300

    Handle different movie release date estimation formats.

--- client/components/filters/ordinal-date/ordinal-date.filter.js
+++ client/components/filters/ordinal-date/ordinal-date.filter.js
@@ -8,10 +8,22 @@ angular.module('mementoMovieApp')
       if (!input) {
         return;
       }
-      var date = new Date(input);
-      var monthYear = $filter('date')(date, 'MMMM yyyy').split(' ');
-      var day = date.getDate();
-      var suffix = (day <= 3) ? suffixes[day] : suffixes[0];
-      return monthYear[0] + ' ' + day + suffix + ', ' + monthYear[1];
+
+      var date;
+
+      if (input.indexOf('9999') === 0) {
+        return 'TBA';
+      } else if (input.indexOf('99') === 5) {
+        return input.slice(0, 4);
+      } else if (input.indexOf('99') === 8) {
+        date = new Date(input.slice(0, 7));
+        return $filter('date')(date, 'MMMM yyyy');
+      } else {
+        date = new Date(input);
+        var monthYear = $filter('date')(date, 'MMMM yyyy').split(' ');
+        var day = date.getDate();
+        var suffix = (day <= 3) ? suffixes[day] : suffixes[0];
+        return monthYear[0] + ' ' + day + suffix + ', ' + monthYear[1];
+      }
     };
   });
